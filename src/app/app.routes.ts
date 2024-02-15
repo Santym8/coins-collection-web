ec86ecd6455cb2cdfbbad2691c0aca60660e99ac
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-02-14 23:52:55 -0500

    Add Login, Register, Header UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    {path: 'register', component: RegisterComponent},
+    
+    { path: '**', component: HomeComponent },
+];
