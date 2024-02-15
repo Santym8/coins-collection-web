cfdfe051a474c8dfb4392214dcb4b4af4cf8ac4c
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-02-15 15:48:02 -0500

    Refactor project folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './_shared/header/header.component';
+import { HeaderComponent } from './_shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/pages/login/login.component';
+import { RegisterComponent } from './user/pages/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
