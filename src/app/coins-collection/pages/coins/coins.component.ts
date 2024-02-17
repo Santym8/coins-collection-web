e2b774244ab736417bec1f7fc5e9f4eef13157f9
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-02-17 00:55:23 -0500

    Add coin-card component with styling

--- src/app/coins-collection/pages/coins/coins.component.ts
+++ src/app/coins-collection/pages/coins/coins.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CoinCardComponent } from '../../components/coin-card/coin-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coins',
   standalone: true,
-  imports: [],
+  imports: [CoinCardComponent, CommonModule],
   templateUrl: './coins.component.html',
   styleUrl: './coins.component.css'
 })
 export class CoinsComponent {
 
+  
 }
